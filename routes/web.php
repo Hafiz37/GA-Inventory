693a0f730f5568f98918007356bc07c00793188b
Muhammad Hafiz Falah <hafizfalah.mhf@gmail.com>
2026-05-15 06:55:04 +0700

    ready to deploy

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
-    use App\Http\Controllers\AssetController;
-    // use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AssetController;
+use Illuminate\Support\Facades\Route;
+
+// Tambahkan ini supaya halaman depan tidak 404
+Route::get('/', function () {
+    return redirect()->route('assets.index');
+});
 
 Route::resource('assets', AssetController::class);
